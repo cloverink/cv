e39618eb8593d7099351db314b2a00645653937e
cloverink <cloverink@gmail.com>
2018-06-10 20:59:35 +0700

    mock main section

--- src/js/base.js
+++ src/js/base.js
@@ -3,14 +3,10 @@ var Main = (function(self) {
   var fn = {}
 
   fn.initModal = function() {
-    console.log(123)
-    $('#myModal').on('shown.bs.modal', function() {
-      $('#myInput').trigger('focus')
-    })
+
   }
 
   my.init = function() {
-    $(document).ready(fn.initModal)
   }
 
   return my
